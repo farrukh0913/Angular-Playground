044e5db3d63767e98fae6a4702ef4a1ff8d3407e
farrukh0913 <farrukh0913@gmail.com>
2024-02-28 17:50:22 +0500

    minor changes

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -8,7 +8,7 @@ import { HttpClient } from '@angular/common/http';
 })
 
 export class ApiService {
-  apiURL: string = 'http://localhost:3000/api';
+  apiURL: string = 'http://localhost:3000';
 
   constructor(private readonly httpClient: HttpClient) { }
 
